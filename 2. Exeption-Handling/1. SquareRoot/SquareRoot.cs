cbb1081341e146e0afa9bd98ef66f64f41218815
bulbrain <georgi_d_g1@abv.bg>
2015-05-12 22:39:42 +0300

    added exeption handling

---
+++ 2. Exeption-Handling/1. SquareRoot/SquareRoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1.SquareRoot
+{
+    class SquareRoot
+    {
+
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            try
+            {
+                int num = Int32.Parse(n);
+                Console.WriteLine(Math.Sqrt(num));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2. Exeption-Handling/2. EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _2.EnterNumbers
+{
+    class EnterNumbers
+    {
+
+        static void Main(string[] args)
+        {
+            int start = 0;
+            int end = 100;
+
+            Console.WriteLine("Insert the start:");
+            try
+            {
+                start = Int32.Parse(Console.ReadLine());
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            Console.WriteLine("Insert the end:");
+            try
+            {
+                end = Int32.Parse(Console.ReadLine());
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            ReadNumbers(start, end);
+        }
+
+        public static void ReadNumbers(int start, int end)
+        {
+            int[] numbers = new int[10];
+            Console.WriteLine("Insert 10 numbers between {0} and {1}", start, end);
+            for (int i = 0; i < 10; i++)
+            {
+                    numbers[i] = Int32.Parse(Console.ReadLine());
+                    if (numbers[i] < start || numbers[i] > end)
+                    {
+                        Console.WriteLine("Invalid number. Try again");
+                        i--;
+                    }
+            }
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
