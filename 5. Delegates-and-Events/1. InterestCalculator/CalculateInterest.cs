493d8daa1820edc1dc0021c1310dc38ac182e0d9
bulbrain <georgi_d_g1@abv.bg>
2015-05-12 22:48:21 +0300

    added delegates and events

---
+++ 5. Delegates-and-Events/1. InterestCalculator/CalculateInterest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public delegate void CalculateInterest(decimal money, double interest, int years);
+
+public class InterestCalculator
+{
+    public decimal Money { get; set; }
+    public double Interest { get; set; }
+    public int Years { get; set; }
+    public CalculateInterest CalculateInterest { get; set; }
+
+    public InterestCalculator(decimal money, double interest, int years, CalculateInterest calculateInterest)
+    {
+        this.Money = money;
+        this.Interest = interest;
+        this.Years = years;
+        this.CalculateInterest = calculateInterest;
+        CalculateInterest call = calculateInterest;
+        call(money, interest, years);
+    }
+    static void GetSimpleInterest(decimal money, double interest, int years)
+    {
+        double A = (double)money * (1 + (interest * years) / 100);
+        string result = A.ToString("0.0000");
+        Console.WriteLine(result);
+    }
+
+    static void GetCompoundInterest(decimal money, double interest, int years)
+    {
+        double A = (double)money * (((1 + (interest / 12)) * (12 * years)) / 100);
+        string result = A.ToString("0.0000");
+        Console.WriteLine(result);
+    }
+    public static void Main(string[] args)
+    {
+        InterestCalculator test1 = new InterestCalculator(500m, 5.6, 10, GetCompoundInterest);
+        InterestCalculator test2 = new InterestCalculator(2500m, 7.2, 15, GetSimpleInterest);
+        Console.ReadLine();
+    }
+}
---
+++ 5. Delegates-and-Events/2. AsynchronousTimer/AsyncTimer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace _2.AsynchronousTimer
+{
+    class AsyncTimer
+    {
+        private static Timer aTimer;
+        public Action Action { get; set; }
+        public int Ticks { get; set; }
+        public int T { get; set; }
+
+
+        public static void Main()
+        {
+            double interval = 1500.0;
+            aTimer = new System.Timers.Timer(interval);
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.AutoReset = false;
+            aTimer.Enabled = true;
+            System.Threading.Thread.Sleep((int)interval * 2);
+            Console.ReadLine();
+        }
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
