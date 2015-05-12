33b7bd35cd81056d80f2a5604ceb8219babf0e17
bulbrain <georgi_d_g1@abv.bg>
2015-05-12 22:41:36 +0300

    added static members and namespaces

---
+++ 2. Static-Memebers-And-Namespaces/1. Point3D/Point3D.cs
@@ -0,0 +1,74 @@
+﻿namespace Point3D
+{
+    using System;
+    public class Point3D
+    {
+        private static readonly Point3D StartingPoint = new Point3D(0, 0, 0);
+
+        private double x;
+        private double y;
+        private double z;
+
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public double X
+        {
+            get { return this.x; }
+            set
+            {
+                if (double.IsNaN(this.x))
+                {
+                    throw new ArgumentException("Must be a number");
+                }
+                this.x = value;
+            }
+        }
+
+        public double Y
+        {
+            get { return this.y; }
+            set
+            {
+                if (double.IsNaN(this.y))
+                {
+                    throw new ArgumentException("Must be a number");
+                }
+                this.y = value;
+            }
+        }
+
+        public double Z
+        {
+            get { return this.z; }
+            set
+            {
+                if (double.IsNaN(this.z))
+                {
+                    throw new ArgumentException("Must be a number");
+                }
+                this.z = value;
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}, {2}", this.X, this.Y, this.Z);
+        }
+
+        static Point3D start()
+        {
+            return StartingPoint;
+        }
+
+        public static void Main(string[] args)
+        {
+            Point3D test = new Point3D(1, 2, 3);
+            Console.WriteLine(test);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2. Static-Memebers-And-Namespaces/2. Distance Calculator/Distance Calculator.cs
@@ -0,0 +1,53 @@
+﻿namespace _2.Distance_Calculator
+{
+
+    using System;
+
+    public static class DistanceCalculator
+    {
+        public static double CalculateDistance(Point3D point1, Point3D point2)
+        {
+            double deltaX = point1.X - point2.X;
+            double deltaY = point1.Y - point2.Y;
+            double deltaZ = point1.Z - point2.Z;
+
+            return Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2) + Math.Pow(deltaZ, 2));
+        }
+
+        public class Point3D
+        {
+            private static readonly Point3D StartingPoint = new Point3D(0, 0, 0);
+
+            public Point3D(double x, double y, double z)
+            {
+                this.X = x;
+                this.Y = y;
+                this.Z = z;
+            }
+
+            public double X { get; set; }
+
+            public double Y { get; set; }
+
+            public double Z { get; set; }
+
+            public static Point3D ReturnToStart()
+            {
+                return StartingPoint;
+            }
+
+            public override string ToString()
+            {
+                return String.Format("[X = {0}, Y = {1}, Z = {2}]", this.X, this.Y, this.Z);
+            }
+
+            static void Main(string[] args)
+            {
+                Point3D first = new Point3D(10, 5, 3);
+                Point3D second = new Point3D(3, 5, 3);
+
+                Console.WriteLine(DistanceCalculator.CalculateDistance(first, second));
+            }
+        }
+    }
+}
